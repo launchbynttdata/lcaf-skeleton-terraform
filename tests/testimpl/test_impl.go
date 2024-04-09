f40df8c2f8d7ebfc8c7be926c5071e2ca02bb9f3
Chris Taylor <132399041+chris11-taylor-nttd@users.noreply.github.com>
2024-04-09 12:45:20 -0500

    Add TODO on default test (#8)

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -10,6 +10,7 @@ import (
 )
 
 func TestComposableComplete(t *testing.T, ctx types.TestContext) {
+	// TODO: Remove this test from your module once you have defined some other tests.
 	t.Run("TestAlwaysSucceeds", func(t *testing.T) {
 		assert.Equal(t, "foo", "foo", "Should always be the same!")
 		assert.NotEqual(t, "foo", "bar", "Should never be the same!")
